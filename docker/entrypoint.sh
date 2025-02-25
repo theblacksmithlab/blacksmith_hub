0f61830279e20e050477aad15077ffecad4455da
0xthecableguy <0xthecableguy@proton.me>
2025-02-25 11:53:30 +0300

    docker init

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ -z "$APP_NAME" ]; then
+    echo "Error: APP_NAME environment variable is required"
+    exit 1
+fi
+
+ln -sfn /app/models /root/projects/whisper.cpp/models
+
+exec ./app
