e56623f7e43a42c08af2d23809d62dec0e04e4fe
0xthecableguy <0xthecableguy@proton.me>
2025-11-30 22:28:05 +0300

    LLMModel usage fixed

--- core/src/message_processing_flow/llm_recommendation_system.rs
+++ core/src/message_processing_flow/llm_recommendation_system.rs
@@ -303,7 +303,7 @@ pub async fn get_lesson_recommendation<T: OpenAIClientInit + QdrantClientInit + 
     );
 
     let result =
-        raw_llm_processing_json(&system_role, &llm_message, app_state, LlmModel::ComplexMini)
+        raw_llm_processing_json(&system_role, &llm_message, app_state, LlmModel::Light)
             .await?;
 
     let parsed_json: Value = serde_json::from_str(&result)?;
